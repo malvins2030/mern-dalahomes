c26c9495517752fd2bae3b4d7e7f64c3f5a0a189
malvins2030 <malvinstech@gmail.com>
2024-02-17 19:14:53 +0300

    complete profile page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
             </Link>
             <Link to='/profile'>
             {currentUser ? (
-              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
+              <img className='rounded-full h-9 w-9 object-cover' src={currentUser.avatar} alt='profile' />
 
             ): 
             (<li className=' text-slate-700 hover:underline'> Sign in</li>
