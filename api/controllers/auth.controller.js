0c1e12f49beca0df9512848b69d41844a2339a08
malvins2030 <malvinstech@gmail.com>
2024-02-17 17:51:13 +0300

    update the header and make the profile page private

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,10 +38,10 @@ export const google = async (req, res, next) => {
     try {
         const user = await User.findOne({ email: req.body.email })
         if (user) {
-            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-            const { password: pass, ...rest } = user.doc;
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const { password: pass, ...rest } = user._doc;
             res
-            .cookie('access token', token,{ httpOnly: true})
+            .cookie('access_token', token,{ httpOnly: true})
             .status(200)
             .json(rest);
 
